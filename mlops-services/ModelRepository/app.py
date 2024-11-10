6e5ee28dc1f7a724a631680469eaaaff609bda9f
MrocznyZdzichu <maksymzawrotny@wp.pl>
2024-11-10 13:41:33 +0100

    Downloading models implemented

--- mlops-services/ModelRepository/app.py
+++ mlops-services/ModelRepository/app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException, Depends, Request
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.responses import HTMLResponse, RedirectResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 import os
 import shutil
@@ -31,3 +31,19 @@ async def delete_model(model_name: str):
         return RedirectResponse(url="/", status_code=303)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error deleting model: {e}")
+
+@app.get("/download_model/{model_name}")
+async def download_model(model_name: str):
+    model_path = os.path.join(_model_store_dir, model_name)
+
+    if not os.path.exists(model_path):
+        raise HTTPException(status_code=404, detail="Model not found.")
+
+    zip_file_path = f"/tmp/{model_name}.zip"
+
+    try:
+        shutil.make_archive(zip_file_path.replace('.zip', ''), 'zip', model_path)
+        return FileResponse(zip_file_path, media_type="application/zip", filename=f"{model_name}.zip")
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error creating zip archive: {e}")
